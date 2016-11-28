9a87e5c3898277974df61496cad4f387870c7686
Nate Ricklin <nate@rickl.in>
2016-11-28 15:30:40 -0800

    get args as env vars

--- cherrypy-server.py
+++ cherrypy-server.py
@@ -1,13 +1,14 @@
 from cherrypy import wsgiserver
-from server import APP
+import server
 
 _API_BASE = os.getenv('API_BASE')  # "the base URL for the game API"
 _API_TOKEN = os.getenv('API_TOKEN') # "the individual API token given to your team"
 
-d = wsgiserver.WSGIPathInfoDispatcher({'/': APP})
+server.API_BASE = _API_BASE
+server.ARGS.API_token = _API_TOKEN
+
+d = wsgiserver.WSGIPathInfoDispatcher({'/': server.APP})
 server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8080), d)
-
-
 
 
 if __name__ == '__main__':
